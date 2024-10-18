52bdc6871f6ad5b53b095029441aff91276f111c
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-10-18 20:35:36 +0200

    add send text to page and edit readme

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 building an app with Spring from "scratch" to understand the power of the framework and of the Boot tool
 
-
+#### PARTS:
+- framework configuration with .xml file
+- moved to a JAVA based configuration
+- app layers structure:
+  - models
+  - service
+  - repository
+- servlet configuration
+  - start a servlet
+  - mapping routes
+  - sending text to pages
--- src/main/java/com/dba/HelloServlet.java
+++ src/main/java/com/dba/HelloServlet.java
@@ -1,13 +1,25 @@
 package com.dba;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet {
 
-    public void service(HttpServletRequest req, HttpServletResponse res) {
-        System.out.println("In Service");
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+        System.out.println("HelloServlet doGet method");
+
+        // res.getWriter().println("Hello World!");
+
+        res.setContentType("text/html");
+
+        PrintWriter out = res.getWriter();
+        out.println("<h2>Hello World!</h2>");
+
     }
 
 }
